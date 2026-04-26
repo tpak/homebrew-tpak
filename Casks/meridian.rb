15b4e444ae264e0274b060ef0b8f24d5032d261a
Chris Tirpak <tpak@users.noreply.github.com>
2026-04-26 20:26:07 +1000

    Update meridian to v2.18.1

--- Casks/meridian.rb
+++ Casks/meridian.rb
@@ -1,6 +1,6 @@
 cask "meridian" do
-  version "2.18.0"
-  sha256 "f37f27bf146a10a2c3ed9c2936db66393187dbafdfb521498379bcd01dc08d96"
+  version "2.18.1"
+  sha256 "f52c1d37d8996142ec0a56c227734e33de27e33b22b0cbf1824c388239b0e259"
 
   url "https://github.com/tpak/Meridian/releases/download/v#{version}/Meridian.app.zip"
   name "Meridian"
